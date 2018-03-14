c8eadd26d8d8e50e3f5aadada4cb3ad434f07ee1
Habib Hadi <habibullah@augmedix.com>
2018-03-14 19:31:27 +0600

    select box with search and select

--- angular/src/app/components/ax-check-uncheck-select-box/ax-check-uncheck-select-box.component.ts
+++ angular/src/app/components/ax-check-uncheck-select-box/ax-check-uncheck-select-box.component.ts
@@ -10,6 +10,15 @@ export class AxCheckUncheckSelectBoxComponent implements OnInit {
   public listItems: Array<string> = ["Baseball", "Basketball", "Cricket", "Field Hockey", "Football", "Table Tennis", "Tennis", "Volleyball"];
   public value: any = ['Baseball'];
 
+  // https://stackoverflow.com/questions/44698660/grouping-items-on-kendo-angular-dropdownlist
+  public people: Array<Object> = [
+    { text: "A", value: 1, groupId: 1 },
+    { text: "B", value: 2, groupId: 1 },
+    { text: "C", value: 3, groupId: 1 },
+    { text: "D", value: 4, groupId: 2 },
+    { text: "E", value: 5, groupId: 2 }
+  ];
+
   constructor() { }
 
   ngOnInit() {
