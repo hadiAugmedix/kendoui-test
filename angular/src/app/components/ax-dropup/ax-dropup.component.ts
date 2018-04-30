47285d43bbd88293ca2b100e5a50213b46c17ac4
Habib Hadi <habibullah@augmedix.com>
2018-04-30 13:52:45 +0600

    scp: popover

--- angular/src/app/components/ax-dropup/ax-dropup.component.ts
+++ angular/src/app/components/ax-dropup/ax-dropup.component.ts
@@ -1,17 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-ax-dropup',
   templateUrl: './ax-dropup.component.html',
   styleUrls: ['./ax-dropup.component.scss']
 })
-export class AxDropupComponent implements OnInit {
+export class AxDropupComponent implements AfterViewInit {
+  private parentNode: any;
 
-  public dropdownOpened: Boolean = true;
+  public dropdownOpened: Boolean = false;
+  public optionData: Object = {
+    valueOne: false,
+    valueTwo: true,
+    valueThree: 'option2',
+    valueFour: 'option2',
+  };
 
-  constructor() { }
+  private wasInside = false;
+  public text = null;
 
-  ngOnInit() {
+  constructor(private _element: ElementRef) { }
+
+  ngAfterViewInit() {
+    this.parentNode = this._element.nativeElement.parentNode;
+  }
+
+  openDropdown() {
+    this.dropdownOpened = !this.dropdownOpened;
+  }
+
+  btnClickHandler($event) {
+    $event.stopPropagation();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside($event) {
+    this.dropdownOpened = false;
   }
 
 }
